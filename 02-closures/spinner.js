4e285535d63ef8417af31f6b29fdfd8ea0545800
Magesh <tkmagesh77@gmail.com>
2018-01-23 11:55:35 +0530

    spinner solution

--- 02-closures/spinner.js
+++ 02-closures/spinner.js
@@ -1,5 +1,17 @@
-var spinner = ...
+var spinner = (function(){
+	var counter = 0;
 
+	function up(){
+		return ++counter;
+	}
+	function down(){
+		return --counter;
+	}
+	return {
+		up : up,
+		down : down
+	}
+})();
 
 
 spinner.up() // => 1
